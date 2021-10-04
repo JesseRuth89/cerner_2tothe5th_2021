0300155ec38ecc75b05291b4a72aabdc14f2999d
JR019593 <jesse.ruth@cerner.com>
2021-10-04 12:00:51 -0500

    Update day4.py

--- day4.py
+++ day4.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
+# cerner_2tothe5th_2021
+
 from multiprocessing import Pool, cpu_count
 import time
 
+# recursive input sanitization
 def user_check(count, prompt, right_answer):
     x = count
+    # getting input
     answer = input(prompt).lower()
+    # basic if statements for correct answer
     if answer not in right_answer and x <= 2:
         print(f"Sorry? '{answer}'?! You have been wrong {x} time(s)...")
         x += 1
@@ -21,14 +26,16 @@ def user_check(count, prompt, right_answer):
         x += 1
         user_check(x, "Do what I said! Now enter it right!: ", right_answer)
 
-
+# pooled process to use all CPUs
 def cpu_killer(x):
+    # only lasts 5 seconds
     t_end = time.time() + 5
     while time.time() < t_end:
         x * x
 
 
 if __name__ == '__main__':
+    # trying to be funny and failing
     user_check(1, "Sigh... please enter true: ", {"true"})
     user_check(1, "Ohhh look a human did something right.. now enter the opposite of true: ", {"false"})
     user_check(1, "Whatever, not like I wanted this either, enter hotdogs: ", {"hotdogs"})
